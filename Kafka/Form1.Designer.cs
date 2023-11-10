c39442c238aad99d0e6c902cac6e969c10584b6a
samdoun641@gmail.com <samdoun641@gmail.com>
2023-11-11 00:42:38 +0500

    I  wanna  die

--- Kafka/Form1.Designer.cs
+++ Kafka/Form1.Designer.cs
@@ -28,20 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstNames = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Names";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lstNames
+            // 
+            this.lstNames.AllowDrop = true;
+            this.lstNames.FormattingEnabled = true;
+            this.lstNames.Location = new System.Drawing.Point(12, 27);
+            this.lstNames.Name = "lstNames";
+            this.lstNames.Size = new System.Drawing.Size(120, 95);
+            this.lstNames.TabIndex = 1;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(138, 26);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 2;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(138, 55);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 23);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Добавить имя";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(138, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 401);
+            this.ClientSize = new System.Drawing.Size(252, 141);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lstNames);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Names";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstNames;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Kafka/Form1.cs
+++ Kafka/Form1.cs
@@ -16,5 +16,71 @@ namespace Kafka
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Участок кода для добавления введённого в поле txtName имени в список lstNames.
+            if (!string.IsNullOrWhiteSpace(txtName.Text) // Эта часть отвечает за то, чтобы поле
+                                                         // не было пустое или не содержало в себе только пробелы.
+                && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Проверка, является ли нажатая клавиша цифрой от 0 до 9.
+            if (e.KeyChar >= 48 && e.KeyChar <= 57) 
+            {
+                // Выводится сообщение о том, какая клавиша была нажата.
+                MessageBox.Show($"Form.KeyPress: ‘{e.KeyChar}' pressed."); // 'нажатый'
+
+                switch (e.KeyChar) // переключение по нажатой клавише.
+                { 
+                    case (char)49: // 1 в Unicode
+                    case (char)52: // 4
+                    case (char)55: // 7
+                        MessageBox.Show($"Form.KeyPress: ‘{e.KeyChar}' consumed."); // 'употребляемый'
+                        e.Handled = true; // Дальнейшая обработка этой клавиши прекращается.
+                        break;
+                } 
+            }
+        }
+        private void Button1_MouseEnter(object sender, EventArgs e) 
+        {
+            Cursor.Hide(); // При наведении на кнопку курсор исчезает.
+        }
+        private void Button1_MouseLeave(object sender, EventArgs e)
+        {
+            Cursor.Show(); // При уведении с кнопки курсор появляется.
+        }
+        private void Button1_Click(object sender, EventArgs e)  
+        {
+            // При нажатии на кнопку курсор перемещается на место
+            // второй кнопки, а текст нажатой кнопки сменится на "Print".
+            Cursor.Position = PointToScreen(button2.Location);
+            button1.Text = "&Print";
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            // Короче, тут просто наоботор с перемещением - со второй на первую.
+            // А при наведении на вторую кнопку курсор меняется на руку.
+            Cursor.Position = PointToScreen(button1.Location);
+            button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            button2.Text = "Print && Close"; // При нажатии на кнопку текст изменится.
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            // При нажатии текст метки изменится. Так же меняется индекс,
+            // который определяет порядок фокусировки при нажатии клавиши Tab -
+            // теперь сначала фокус будет на метке, а потом на кнопке.
+            label1.Text = "&Print";
+            label1.TabIndex = 9;
+            button1.TabIndex = 10;
+        }
     }
 }

bf19fa5d4ee20f74c694a0b7e4fadcb0ad245532
samdoun641@gmail.com <samdoun641@gmail.com>
2023-11-12 18:19:30 +0500

    I love POKEMON!!!

--- Kafka/Form1.Designer.cs
+++ Kafka/Form1.Designer.cs
@@ -28,20 +28,41 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(100, 100);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(52, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(592, 401);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(445, 293);
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Pokemon Game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Kafka/Form1.cs
+++ Kafka/Form1.cs
@@ -12,9 +12,43 @@ namespace Kafka
 {
     public partial class Form1 : Form
     {
+        int x = 100;
+        int y = 100;
         public Form1()
         {
             InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+            //PictureBox player = new PictureBox();
+            //player.Image = Properties.Resources.arrow;
+            pictureBox1.Location = new System.Drawing.Point(x, y);
+            this.Controls.Add(pictureBox1);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    y -= 10;
+                    break;
+                case Keys.Down:
+                    y += 10; 
+                    break;
+                case Keys.Left:
+                    x -= 10;
+                    break;
+                case Keys.Right: 
+                    x += 10;
+                    break;
+            }
+            foreach (Control control in this.Controls)
+            {
+                if (control is PictureBox)
+                {
+                    control.Location = new System.Drawing.Point(x, y);
+                }
+            }
         }
     }
 }

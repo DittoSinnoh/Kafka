5a16256f6e790cf5ff581d6757508ac7228d09a5
samdoun641@gmail.com <samdoun641@gmail.com>
2023-11-30 12:25:17 +0500

    Всё изменилось вдруг

--- Kafka/Form1.cs
+++ Kafka/Form1.cs
@@ -16,5 +16,30 @@ namespace Kafka
         {
             InitializeComponent();
         }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double Xmin = double.Parse(txtXmin.Text);
+            double Xmax = double.Parse(txtXmax.Text);
+            double Step = double.Parse(txtStep.Text);
+            int count = (int)Math.Ceiling((Xmax - Xmin) / Step) + 1;
+            double[] x = new double[count];
+            double[] y1 = new double[count];
+            double[] y2 = new double[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                x[i] = Xmin + Step * i;
+                y1[i] = Math.Sin(x[i]);
+                y2[i] = Math.Cos(x[i]);
+            }
+            chart1.ChartAreas[0].AxisX.Minimum = Xmin;
+            chart1.ChartAreas[0].AxisX.Maximum = Xmax;
+
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = Step;
+
+            chart1.Series[0].Points.DataBindXY(x, y1);
+            chart1.Series[1].Points.DataBindXY(x, y1);
+        }
     }
 }

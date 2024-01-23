b578da13dc822593813fff633f52d69b72fda373
Admin <Ulia@gmail.com>
2024-01-23 16:42:39 +0500

    Hell is Forever

--- Kafka/Form1.cs
+++ Kafka/Form1.cs
@@ -16,5 +16,25 @@ namespace Kafka
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
